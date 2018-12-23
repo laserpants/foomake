90c857c0c47a6acb4d8826d05e0361b345b92619
Johannes Hildén <hildenjohannes@gmail.com>
2018-12-23 11:35:07 +0300

    remove redundant imports

--- lib/Data/CMake/Types/Targets/Executable.hs
+++ lib/Data/CMake/Types/Targets/Executable.hs
@@ -3,13 +3,10 @@ module Data.CMake.Types.Targets.Executable
   ( Executable(..)
   ) where
 
-import Control.Applicative ((<|>))
 import Data.Aeson
 import Data.CMake.Types.Targets.IncludeDirectory
 import Data.CMake.Types.Targets.LinkLibrary
 import Data.Text
-
-import Data.Aeson.Types
 
 data Executable = Executable
   { includeDirs :: ![IncludeDirectory]
